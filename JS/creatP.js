45e8cf319e5b461a99a01ac05b1ab0560c1ae869
Hudson Retonde <hudsonsretonde@gmail.com>
2022-06-14 11:21:13 -0300

    section elenco finalizada e adcionei JS para manipular a div acionada pelo button 'extra'.

---
+++ JS/creatP.js
@@ -0,0 +1,90 @@
+var btn = document.querySelector('#mostrarEsconder');
+var container = document.querySelector('._f-18-extra');
+
+btn.addEventListener('click', function(){
+
+    if(container.style.display === 'block'){
+        container.style.display = 'none';
+    } else {
+        container.style.display = 'block';
+    }
+
+});
+
+var botao = document.querySelector('#f14');
+var pMais = document.querySelector('._f-14-extra');
+
+botao.addEventListener('click', function(){
+
+    if(pMais.style.display === 'block'){
+        pMais.style.display = 'none';
+    } else {
+        pMais.style.display = 'block';
+    }
+
+});
+
+var button = document.querySelector('#sukhoi');
+var sMais = document.querySelector('._sukhoi-extra');
+
+button.addEventListener('click', function(){
+
+    if(sMais.style.display === 'block'){
+        sMais.style.display = 'none';
+    } else {
+        sMais.style.display = 'block';
+    }
+
+});
+
+var btn51 = document.querySelector('#p51');
+var p51Mais = document.querySelector('._p51-extra');
+
+btn51.addEventListener('click', function(){
+
+    if(p51Mais.style.display === 'block'){
+        p51Mais.style.display = 'none';
+    } else {
+        p51Mais.style.display = 'block';
+    }
+
+});
+
+var btnDark = document.querySelector('#dark');
+var darkMais = document.querySelector('._dark-extra');
+
+btnDark.addEventListener('click', function(){
+
+    if(darkMais.style.display === 'block'){
+        darkMais.style.display = 'none';
+    } else {
+        darkMais.style.display = 'block';
+    }
+
+});
+
+var btnH2r = document.querySelector('#h2r');
+var h2rMais = document.querySelector('._h2r-extra');
+
+btnH2r.addEventListener('click', function(){
+
+    if(h2rMais.style.display === 'block'){
+        h2rMais.style.display = 'none';
+    } else {
+        h2rMais.style.display = 'block';
+    }
+
+});
+
+var btnGpz = document.querySelector('#gpz');
+var gpzMais = document.querySelector('._gpz-extra');
+
+btnGpz.addEventListener('click', function(){
+
+    if(gpzMais.style.display === 'block'){
+        gpzMais.style.display = 'none';
+    } else {
+        gpzMais.style.display = 'block';
+    }
+
+});
